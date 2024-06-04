a6c31479c3594b5ba5303aac652aa615474b4f63
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-06-04 09:34:46 +0700

    example your first components

---
+++ example-your-first-component/src/components/Information/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Information = () => {
+	return (
+		<div>
+			<h1>Pizza Margherita</h1>
+			<p>Tomato, mozarella, mushrooms, and onion</p>
+		</div>
+	);
+};
+
+export default Information;
