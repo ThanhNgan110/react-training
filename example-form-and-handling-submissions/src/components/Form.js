3e7f8fcdcf467af5aad5aa68b63bca926f95e7c2
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-06-06 17:37:10 +0700

    Update example form and handle submission

--- example-form-and-handling-submissions/src/components/Form.js
+++ example-form-and-handling-submissions/src/components/Form.js
@@ -1,13 +1,33 @@
+import { useState } from "react";
 const Form = () => {
+	const [description, setDescription] = useState("");
+	const [quantity, setQuantity] = useState(0);
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!description) return;
+		const newItem = { id: Date.now(), description, quantity, packed: false };
+		console.log(newItem);
+	};
+
 	return (
-		<form className="add-form">
+		<form className="add-form" onSubmit={handleSubmit}>
 			<h3>What do you need for your 😍 trip?</h3>
-			<select>
+			<select
+				value={quantity}
+				onChange={(e) => setQuantity(Number(e.target.value))}
+			>
 				{Array.from({ length: 10 }, (_, i) => i + 1).map((number) => (
-					<option value={number}>{number}</option>
+					<option value={number} key={number}>
+						{number}
+					</option>
 				))}
 			</select>
-			<input type="text" placeholder="Item..." />
+			<input
+				type="text"
+				placeholder="Item..."
+				value={description}
+				onChange={(e) => setDescription(e.target.value)}
+			/>
 			<button>Add</button>
 		</form>
 	);
