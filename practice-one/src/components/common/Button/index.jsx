10b88126596862d14c6e2756801672bf0b87fdee
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-06-17 11:34:18 +0700

    Implement component button

---
+++ practice-one/src/components/common/Button/index.jsx
@@ -0,0 +1,16 @@
+import './index.css';
+import '../../../styles/base/flex.css';
+
+const Button = ({ className, text, isDisabled, icon, onClick }) => {
+  return (
+    <button
+      className={`align-center btn ${className} `}
+      onClick={onClick}
+      disabled={isDisabled}>
+      {icon && <img src={icon} alt="icon" className="button-icon" />}
+      {text && <span>{text}</span>}
+    </button>
+  );
+};
+
+export default Button;
