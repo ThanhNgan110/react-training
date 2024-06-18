8ba09652569c11421836ada835db89a1b823cddc
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-06-18 15:32:44 +0700

    Add home page

--- practice-one/src/App.jsx
+++ practice-one/src/App.jsx
@@ -1,34 +1,9 @@
-import Title from './components/Title';
-// import NumberTask from './components/NumberTask';
-import Button from './components/common/Button';
-import iconAdd from '../src/assets/images/iconButton/iconAdd.png';
-import Form from './components/Form';
-import '../src/styles/abstracts/index.css';
-
-import { useState } from 'react';
+import HomePage from './views/HomePage';
 
 const App = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleShowForm = () => {
-    setIsOpen((prev) => !prev);
-  };
-
   return (
     <>
-      <Title />
-
-      {/* <NumberTask /> */}
-      {!isOpen && (
-        <Button
-          className="btn-toggle"
-          text="Add Task"
-          icon={iconAdd}
-          onClick={handleShowForm}
-        />
-      )}
-
-      {isOpen && <Form />}
+      <HomePage />
     </>
   );
 };
