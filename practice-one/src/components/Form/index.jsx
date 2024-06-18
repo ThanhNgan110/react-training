15aaad743f975098b50c2e8f591d25da413276c1
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-06-18 15:31:28 +0700

    Update form

--- practice-one/src/components/Form/index.jsx
+++ practice-one/src/components/Form/index.jsx
@@ -1,15 +1,44 @@
+import { useState } from 'react';
 import Input from '../common/Input';
 import Button from '../common/Button';
 import './index.css';
 
-const Form = () => {
+const Form = ({ onAddTask, children }) => {
+  const [taskName, setTaskName] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const task = {
+      id: self.crypto.randomUUID(),
+      taskName: taskName,
+      description: description,
+    };
+    setTaskName('');
+    setDescription('');
+    onAddTask(task);
+    // localStorage.setItem('tasks', JSON.stringify(task));
+  };
+  
   return (
-    <form className="form">
-      <Input type="text" placeHolder="Task Name" className="input-task" />
-      <Input type="text" placeHolder="Description" className="input-desc" />
+    <form className="form" onSubmit={handleSubmit}>
+      <Input
+        type="text"
+        placeHolder="Task Name"
+        className="input-task"
+        value={taskName}
+        onChange={(e) => setTaskName(e.target.value)}
+      />
+      <Input
+        type="text"
+        placeHolder="Description"
+        className="input-desc"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <div className="btn-group">
-        <Button text="Cancel" className="btn-cancel" />
-        <Button text="Add Task" className="btn-add" />
+        {children}
+        <Button text="Add Task" className="btn-secondary" />
       </div>
     </form>
   );
