f38458cdf3ad239586d8f133e7f30fe174b64aa2
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-06-19 15:08:51 +0700

    Update component list item

--- practice-one/src/components/ListItem/index.jsx
+++ practice-one/src/components/ListItem/index.jsx
@@ -1,6 +1,15 @@
+import Button from '../common/Button';
+import iconEdit from '../../assets/images/icons/icon-edit.png';
+import './index.css';
+
 const ListItem = ({ children }) => {
-  
-  return <li>{children}</li>;
+
+  return (
+    <li className="list-item">
+      {children}
+     <Button icon={iconEdit} className='btn-edit' alt='edit'/>
+    </li>
+  );
 };
 
 export default ListItem;
