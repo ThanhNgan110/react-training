8a3c308a55285678e2e5dd9b6204780eb3b991a3
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-06-21 10:37:39 +0700

    Add props func edit task

--- practice-one/src/components/ListItem/index.jsx
+++ practice-one/src/components/ListItem/index.jsx
@@ -3,14 +3,14 @@ import iconEdit from '../../assets/images/icons/icon-edit.png';
 import iconRemove from '../../assets/images/icons/icon-remove.png';
 import './index.css';
 
-const ListItem = ({ children, task, onRemove }) => {
+const ListItem = ({ children, task, onRemove, onEdit }) => {
   return (
     <li className="list-item">
       <div className='flex-row'>
         <Button icon={iconRemove} className="btn-remove" alt="remove" onClick={()=> onRemove(task.id)} />
         {children}
       </div>
-      <Button icon={iconEdit} className="btn-edit" alt="edit" />
+      <Button icon={iconEdit} className="btn-edit" alt="edit" onClick={()=> onEdit(task.id)} />
     </li>
   );
 };
