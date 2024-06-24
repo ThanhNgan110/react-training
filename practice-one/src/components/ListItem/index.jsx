7e86a6c60cb24e83696be1979f42d0a7153cbc60
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-06-24 18:09:36 +0700

    Update list item

--- practice-one/src/components/ListItem/index.jsx
+++ practice-one/src/components/ListItem/index.jsx
@@ -1,16 +1,22 @@
 import Button from '../common/Button';
-import iconEdit from '../../assets/images/icons/icon-edit.png';
-import iconRemove from '../../assets/images/icons/icon-remove.png';
 import './index.css';
 
-const ListItem = ({ children, task, onRemove, onEdit }) => {
+const ListItem = ({ children, onRemove, onEdit, task }) => {
   return (
     <li className="list-item">
-      <div className='flex-row'>
-        <Button icon={iconRemove} className="btn-remove" alt="remove" onClick={()=> onRemove(task.id)} />
+      <div className="flex-row">
+        <Button
+          icon="icon-remove"
+          className="btn-remove"
+          onClick={() => onRemove(task.id)}
+        />
         {children}
       </div>
-      <Button icon={iconEdit} className="btn-edit" alt="edit" onClick={()=> onEdit(task.id)} />
+      <Button
+        icon="icon-edit"
+        className="btn-edit"
+        onClick={() => onEdit(task.id)}
+      />
     </li>
   );
 };
