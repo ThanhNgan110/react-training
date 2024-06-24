bddbc7950c59741a9dc424d71a106c5ced9d73cc
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-06-24 18:09:01 +0700

    Add helper for local storage

---
+++ practice-one/src/helper/localStorage.js
@@ -0,0 +1,11 @@
+const getLocalStorage = (item) => {
+
+  return localStorage.getItem(item);
+};
+
+const setLocalStorage = (item, updatedTask) => {
+    
+  return localStorage.setItem(item, updatedTask);
+};
+
+export { getLocalStorage, setLocalStorage };
