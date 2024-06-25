8add4c2a5875d10d6f3c40a4c7697a520c972764
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-06-25 15:35:45 +0700

    Update task list

--- practice-one/src/components/TaskList/index.jsx
+++ practice-one/src/components/TaskList/index.jsx
@@ -1,16 +1,32 @@
 import List from '../List';
 import ListItem from '../ListItem';
 import TaskContent from '../TaskContent';
+import Button from '../common/Button';
+import Form from '../Form';
 
-const TaskList = ({ tasks, onRemoveTask, onEditTask }) => {
+const TaskList = ({ tasks, onRemoveTask, onEditTask, onCancel, onSave }) => {
   return (
     <>
-      <List tasks={tasks}>
+      <List>
         {tasks.map((task) => (
-          <ListItem key={task.id} primaryBtnOnClick={onRemoveTask} secondaryBtnOnClick={onEditTask}>
-            <TaskContent
-              task={task}
-            />
+          <ListItem key={task.id}>
+            {task.active ? (
+              <Form task={task} onCancel={onCancel} onSave={onSave}/>
+            ) : (
+              <>
+                <div className="flex-row">
+                  <Button
+                    classNameBtn="btn-remove"
+                    iconClassName="icon-remove"
+                    onClick={() => onRemoveTask(task.id)}></Button>
+                  <TaskContent task={task} />
+                </div>
+                <Button
+                  classNameBtn="btn-edit"
+                  iconClassName="icon-edit"
+                  onClick={() => onEditTask(task.id)}></Button>
+              </>
+            )}
           </ListItem>
         ))}
       </List>
