d5e4119a038453284cbcd1753d5055778bcd4cb7
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-06-25 09:47:48 +0700

    Update component task list

--- practice-one/src/components/TaskList/index.jsx
+++ practice-one/src/components/TaskList/index.jsx
@@ -1,28 +1,20 @@
 import List from '../List';
 import ListItem from '../ListItem';
 import TaskContent from '../TaskContent';
-import Form from '../Form';
 
 const TaskList = ({ tasks, onRemoveTask, onEditTask }) => {
-  
   return (
     <>
       <List tasks={tasks}>
-        {tasks.map((task) => {
-          {
-            task.active ? (
-              <Form />
-            ) : (
-              <ListItem key={task.id}>
-                <TaskContent
-                  task={task}
-                  onRemove={onRemoveTask}
-                  onEdit={onEditTask}
-                />
-              </ListItem>
-            );
-          }
-        })}
+        {tasks.map((task) => (
+          <ListItem key={task.id}>
+            <TaskContent
+              task={task}
+              onRemove={onRemoveTask}
+              onEdit={onEditTask}
+            />
+          </ListItem>
+        ))}
       </List>
     </>
   );
