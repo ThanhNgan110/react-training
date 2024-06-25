536d0cd0b69e7d3e5dc2a947995f359157ad99fd
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-06-25 15:35:20 +0700

    Update list item

--- practice-one/src/components/ListItem/index.jsx
+++ practice-one/src/components/ListItem/index.jsx
@@ -1,34 +1,7 @@
-import Button from '../common/Button';
 import './index.css';
 
-const ListItem = ({
-  children,
-  primaryBtnClassName,
-  primaryIconClassName,
-  secondaryBtnClassName,
-  secondaryIconClassName,
-  primaryBtnText,
-  secondaryBtnText,
-  primaryBtnOnClick,
-  secondaryBtnOnClick,
-}) => {
-  return (
-    <li className="list-item">
-      <Button
-        iconClassName={primaryIconClassName}
-        className={primaryBtnClassName}
-        onClick={primaryBtnOnClick}
-        text={primaryBtnText}
-      />
-      {children}
-      <Button
-        iconClassName={secondaryIconClassName}
-        className={secondaryBtnClassName}
-        onClick={secondaryBtnOnClick}
-        text={secondaryBtnText}
-      />
-    </li>
-  );
+const ListItem = ({ children }) => {
+  return <li className="list-item">{children}</li>;
 };
 
 export default ListItem;
