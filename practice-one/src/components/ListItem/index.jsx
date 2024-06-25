ce8b22f9ce71ce3e1ccda817ea7b80c2b82aa775
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-06-25 08:58:32 +0700

    Add component list item

--- practice-one/src/components/ListItem/index.jsx
+++ practice-one/src/components/ListItem/index.jsx
@@ -1,21 +1,28 @@
 import Button from '../common/Button';
 import './index.css';
 
-const ListItem = ({ children, onRemove, onEdit, task }) => {
+const ListItem = ({
+  children,
+  primaryBtnClassName,
+  primaryIconClassName,
+  secondaryClassName,
+  secondaryIconClassName,
+}) => {
+
   return (
     <li className="list-item">
       <div className="flex-row">
         <Button
-          icon="icon-remove"
-          className="btn-remove"
-          onClick={() => onRemove(task.id)}
+          iconClassName={primaryIconClassName}
+          // className="btn-remove"
+          onClick={primaryBtnClassName}
         />
         {children}
       </div>
       <Button
-        icon="icon-edit"
-        className="btn-edit"
-        onClick={() => onEdit(task.id)}
+        iconClassName={secondaryIconClassName}
+        // className="btn-edit"
+        onClick={secondaryClassName}
       />
     </li>
   );
