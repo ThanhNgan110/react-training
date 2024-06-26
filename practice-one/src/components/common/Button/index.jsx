6ad5ea98b416513a98a098972c5f0bdec8acc597
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-06-26 11:29:30 +0700

    Add props type

--- practice-one/src/components/common/Button/index.jsx
+++ practice-one/src/components/common/Button/index.jsx
@@ -1,14 +1,23 @@
 import './index.css';
 import '../../../styles/flex.css';
 
-const Button = ({ classNameBtn, text, isDisabled, iconClassName, onClick }) => {
-  
+const Button = ({
+  classNameBtn,
+  text,
+  isDisabled,
+  iconClassName,
+  onClick,
+  type,
+}) => {
   return (
     <button
       className={`align-center btn ${classNameBtn} `}
       onClick={onClick}
-      disabled={isDisabled}>
-      {iconClassName && <span className={`icon icon-small ${iconClassName}`}></span>}
+      disabled={isDisabled}
+      type={type}>
+      {iconClassName && (
+        <span className={`icon icon-small ${iconClassName}`}></span>
+      )}
       {text && text}
     </button>
   );
