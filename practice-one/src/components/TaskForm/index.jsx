c4083c41748abb55a9e4dafdf8ba7719ad084dc1
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-06-26 13:43:29 +0700

    Add task form

---
+++ practice-one/src/components/TaskForm/index.jsx
@@ -0,0 +1,76 @@
+import { useState, useEffect } from 'react';
+import Input from '../common/Input';
+import Button from '../common/Button';
+import Form from '../Form';
+
+const TaskForm = ({ data, onSubmit, onCancel, textBtnPrimary, textBtnSecondary }) => {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+
+  useEffect(() => {
+    const {
+      name = '',
+      description = ''
+    } = data || {};
+    setName(name);
+    setDescription(description);
+
+    return () => {
+      setName('');
+      setDescription('');
+    };
+
+  }, [data]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const task = {
+      id: data?.id || self.crypto.randomUUID(),
+      name: name,
+      description: description,
+    };
+    setName('');
+    setDescription('');
+    onSubmit(task);
+  };
+
+  const handleChangeName = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleChangeDescription = (e) => {
+    setDescription(e.target.value);
+  };
+
+  return (
+    <Form
+      handleSubmit={handleSubmit}
+      content={
+        <>
+          <Input
+            type="text"
+            placeHolder="Task Name"
+            className="input-task"
+            value={name}
+            onChange={handleChangeName}
+          />
+          <Input
+            type="text"
+            placeHolder="Description"
+            className="input-desc"
+            value={description}
+            onChange={handleChangeDescription}
+          />
+        </>
+      }
+      footer={
+        <div className="btn-group">
+          <Button text={textBtnPrimary} classNameBtn="btn-primary" type="button" onClick={onCancel} />
+          <Button text={textBtnSecondary} classNameBtn="btn-secondary" type="submit" />
+        </div>
+      }
+    />
+  );
+};
+
+export default TaskForm;
