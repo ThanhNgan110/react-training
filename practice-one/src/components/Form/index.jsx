9c23b3322dadb09375dbe56ea7de3a53876f5251
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-06-26 13:42:17 +0700

    Import task form

--- practice-one/src/components/Form/index.jsx
+++ practice-one/src/components/Form/index.jsx
@@ -1,53 +1,10 @@
-import { useState } from 'react';
-import Input from '../common/Input';
-import Button from '../common/Button';
 import './index.css';
 
-const Form = ({ task, onAddTask, onCancel, onSave }) => {
-  const [taskName, setTaskName] = useState(task?.taskName || '');
-  const [description, setDescription] = useState(task?.description || '');
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const task = {
-      id: self.crypto.randomUUID(),
-      taskName: taskName,
-      description: description,
-    };
-    setTaskName('');
-    setDescription('');
-    onAddTask(task);
-  };
-
-  const handleChangeName = (e) => {
-    setTaskName(e.target.value);
-  };
-
-  const handleChangeDescription = (e) => {
-    setDescription(e.target.value);
-  };
-
+const Form = ({ handleSubmit, content, footer }) => {
   return (
     <form className="form" onSubmit={handleSubmit}>
-      <Input
-        type="text"
-        placeHolder="Task Name"
-        className="input-task"
-        value={taskName}
-        onChange={handleChangeName}
-      />
-      <Input
-        type="text"
-        placeHolder="Description"
-        className="input-desc"
-        value={description}
-        onChange={handleChangeDescription}
-      />
-      <div className="btn-group">
-        <Button text="Cancel" classNameBtn="btn-primary" onClick={onCancel} />
-        {task?.active  ? (<Button text="Save" classNameBtn="btn-secondary"/>) :
-           (<Button text="Add Task" classNameBtn="btn-secondary" type="submit" />)}
-      </div>
+      <div className="form-content">{content}</div>
+      <div className="form-footer">{footer}</div>
     </form>
   );
 };
--- practice-one/src/components/TaskList/index.jsx
+++ practice-one/src/components/TaskList/index.jsx
@@ -1,16 +1,17 @@
 import List from '../List';
 import ListItem from '../ListItem';
 import TaskContent from '../TaskContent';
-import Form from '../Form';
+import TaskForm from '../TaskForm';
 
-const TaskList = ({ tasks, onRemoveTask, onEditTask, onCancel, onSave, selectedTaskId }) => {
+const TaskList = ({ tasks, onRemoveTask, onEditTask, onCancel, onSubmit, selectedTaskId }) => {
+
   return (
     <>
       <List>
         {tasks.map((task) => (
           <ListItem key={task.id}>
             {selectedTaskId === task.id ? (
-              <Form task={task} onCancel={onCancel} onSave={onSave} />
+              <TaskForm data={task} onCancel={onCancel} onSubmit={onSubmit} textBtnSecondary='Save' textBtnPrimary='Cancel'/>
             ) : (
               <TaskContent
                 task={task}
