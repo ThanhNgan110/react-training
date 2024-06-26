9686c4bc73ecd14c2699ea4796a746ecdbfa2e35
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-06-26 15:39:30 +0700

    Update show number task

--- practice-one/src/views/Todo/components/NumberTask/index.jsx
+++ practice-one/src/views/Todo/components/NumberTask/index.jsx
@@ -1,16 +1,18 @@
 const NumberTask = ({ tasks }) => {
+  if (tasks.length === 0) {
+    return null;
+  }
+
   const taskWord = tasks.length > 1 ? 'tasks' : 'task';
 
   return (
     <>
-      {tasks.length >= 1 && (
-        <div className="align-center">
-          <span className="icon icon-small icon-complete"></span>
-          <p className="number-task">
-            {tasks.length} {taskWord}
-          </p>
-        </div>
-      )}
+      <div className="align-center">
+        <span className="icon icon-small icon-complete"></span>
+        <p className="number-task">
+          {tasks.length} {taskWord}
+        </p>
+      </div>
     </>
   );
 };
