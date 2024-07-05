e86d6f74436c0a96e760604785f561ca07ac299b
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-07-05 11:05:28 +0700

    Add component btn

---
+++ practice-two/src/components/Button/index.jsx
@@ -0,0 +1,13 @@
+const Button = ({ className, type, onClick, children }) => {
+  return (
+    <button
+      className={className}
+      type={type}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
