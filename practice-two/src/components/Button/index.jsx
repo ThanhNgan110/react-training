ef20469539b73c7b7a58c86fe500f723aa995845
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-07-08 09:35:03 +0700

    Update props variant button

--- practice-two/src/components/Button/index.jsx
+++ practice-two/src/components/Button/index.jsx
@@ -1,10 +1,16 @@
+import clsx from 'clsx';
 import './index.css';
 
-const Button = ({ className, type, onClick, children }) => {
+const Button = ({ variant, type, disabled, onClick, children }) => {
   return (
     <button
-      className={`btn ${className}`}
+      className={clsx('btn', {
+        'btn-primary': variant === 'primary',
+        'btn-secondary': variant === 'secondary',
+        'btn-link': variant === 'link'
+      })}
       type={type}
+      disabled={disabled}
       onClick={onClick}
     >
       {children}
