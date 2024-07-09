6210bea751443f4520ac4f87fec31adae9447c9b
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-07-09 10:08:57 +0700

    Update component select

--- practice-two/src/components/Select/index.jsx
+++ practice-two/src/components/Select/index.jsx
@@ -2,15 +2,13 @@ import './index.css';
 
 const Select = ({ name, onChange, children }) => {
   return (
-    <div>
-      <select
-        className="select"
-        name={name}
-        onChange={onChange}
-      >
-        {children}
-      </select>
-    </div>
+    <select
+      className="select"
+      name={name}
+      onChange={onChange}
+    >
+      {children}
+    </select>
   );
 };
 
