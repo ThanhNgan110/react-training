dad7bd838e0c3a0dff1d7ced49692b560ab3caaf
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-07-11 11:27:49 +0700

    Update header

--- practice-two/src/layouts/Header/index.jsx
+++ practice-two/src/layouts/Header/index.jsx
@@ -7,10 +7,10 @@ import './index.css';
 
 const Header = () => {
   return (
-    <div className="flex align-items-center justify-content-center wrapper-header">
+    <nav className="flex align-items-center justify-content-center wrapper-header">
       <Logo />
       <NavList />
-    </div>
+    </nav>
   );
 };
 
