0d293f010d69dd3ff07f5ed537203a3c6fd51874
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-07-14 14:16:00 +0700

    Update render sidebar

--- practice-two/src/layouts/SideBar/index.jsx
+++ practice-two/src/layouts/SideBar/index.jsx
@@ -1,4 +1,7 @@
-// Import component
+// Import react
+import { useEffect, useState } from 'react';
+
+// Import components
 import Trademarks from './Trademarks';
 import Prices from './Prices';
 import Colors from './Colors';
@@ -6,12 +9,34 @@ import Colors from './Colors';
 // Import css
 import './index.css';
 
+// Import api
+import { getProductTypes } from '../../services/Products/filterService';
+
 const SideBar = () => {
+  const [productData, setProductData] = useState({
+    types: [],
+    colors: [],
+    maxPrice: 0
+  });
+
+  useEffect(() => {
+    const fetchProductTypes = async () => {
+      // Call loading icon if needed
+      const { data, isError } = await getProductTypes();
+      if (!isError) {
+        setProductData(data);
+      }
+      // Hide loading icon if needed
+    };
+
+    fetchProductTypes();
+  }, []);
+
   return (
     <div className="sidebar">
-      <Trademarks />
-      <Prices />
-      <Colors />
+      <Trademarks types={productData.types} />
+      <Prices maxPrice={productData.maxPrice} />
+      <Colors colors={productData.colors} />
     </div>
   );
 };
