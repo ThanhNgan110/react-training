62d37c415514b7be573254847b45fa47dd195ed9
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-07-15 15:36:28 +0700

    Update sidebar

--- practice-two/src/layouts/SideBar/index.jsx
+++ practice-two/src/layouts/SideBar/index.jsx
@@ -2,23 +2,24 @@
 import Trademarks from './Trademarks';
 import Prices from './Prices';
 import Colors from './Colors';
+import ListGroup from '../../components/ListGroup';
 
 // Import css
 import './index.css';
 
 const SideBar = ({ productData }) => {
   return (
-    <div className="sidebar">
-      <div className="sidebar-item">
+    <ListGroup className="sidebar" horizontal={false}>
+      <ListGroup.Item className="sidebar-item">
         <Trademarks types={productData.types} />
-      </div>
-      <div className="sidebar-item">
+      </ListGroup.Item>
+      <ListGroup.Item className="sidebar-item">
         <Prices maxPrice={productData.maxPrice} />
-      </div>
-      <div className="sidebar-item">
+      </ListGroup.Item>
+      <ListGroup.Item className="sidebar-item">
         <Colors colors={productData.colors} />
-      </div>
-    </div>
+      </ListGroup.Item>
+    </ListGroup>
   );
 };
 
