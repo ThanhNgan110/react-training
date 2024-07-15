5b200e5113be93acb10b0155ed73aa56d6bf4cb4
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-07-15 15:15:10 +0700

    Update change file name

---
+++ practice-two/src/pages/Home/index.jsx
@@ -0,0 +1,43 @@
+// Import react
+import { useState, useEffect } from 'react';
+
+//Import component
+import Header from '../../layouts/Header';
+import SideBar from '../../layouts/SideBar';
+
+// Import css
+import './index.css';
+
+// Import api
+import { getProductTypes } from '../../services/Products/filter-service';
+
+const HomePage = () => {
+  const [productData, setProductData] = useState({
+    types: [],
+    colors: [],
+    maxPrice: 0
+  });
+
+  useEffect(() => {
+    const fetchProductTypes = async () => {
+      // Call loading icon if needed
+      const { data, isError } = await getProductTypes();
+      if (!isError) {
+        setProductData(data);
+      }
+      // Hide loading icon if needed
+    };
+
+    fetchProductTypes();
+  }, []);
+  return (
+    <>
+      <Header />
+      <main className="wrapper-main">
+        <SideBar productData={productData} />
+      </main>
+    </>
+  );
+};
+
+export default HomePage;
