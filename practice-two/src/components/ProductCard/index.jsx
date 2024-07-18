e24e1662dad926acf91a0b33160a60235c8fe085
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-07-18 16:53:28 +0700

    Update css product card

--- practice-two/src/components/ProductCard/index.jsx
+++ practice-two/src/components/ProductCard/index.jsx
@@ -3,32 +3,65 @@ import Image from '../Image';
 import StarRating from '../StarRating';
 import Button from '../Button';
 
+import './index.css';
+
 const ProductCard = ({ product }) => {
-  return (
-      product?.map(item => (
-        <article
-          className="d-flex"
-          key={item.id}
+  return product?.map(item => (
+    <article
+      className="d-flex"
+      key={item.id}
+    >
+      <div className="box-image">
+        <Image
+          src={item.image[1]}
+          alt="product"
+        />
+        <span className="badge"></span>
+      </div>
+      <div className="d-flex flex-column product-content">
+        <Text
+          as="h3"
+          variant="primary"
         >
-          <Image
-            src={item.image[1]}
-            alt="product"
-          />
-          <div>
+          {item.name}
+        </Text>
+        <div className="d-flex align-items-center">
+          <StarRating starFull={item.reviews} />
+          <Text
+            as="p"
+            className="review"
+          >
+            {item.reviews} reviews
+          </Text>
+          <Button variant="link">Submit a review</Button>
+        </div>
+        <div className="d-flex align-item-center">
+          <Text
+            as="p"
+            className="price"
+          >
+            $ {item.price}
+          </Text>
+          <div className="d-flex group">
             <Text
-              as="h3"
-              variant="primary"
+              as="p"
+              className="discount"
             >
-              {item.name}
+              $534,33
             </Text>
-            <StarRating starFull={item.reviews} />
-            <Button variant="link">Submit a review</Button>
-            <Text as="p">{item.price}</Text>
-            <Text as="p">{item.description}</Text>
+            <Text as="p" className="percentage">24% Off</Text>
           </div>
-        </article>
-      ))
-  );
+        </div>
+        <Text
+          as="p"
+          variant="secondary"
+          ellipsis
+        >
+          {item.description}
+        </Text>
+      </div>
+    </article>
+  ));
 };
 
 export default ProductCard;
