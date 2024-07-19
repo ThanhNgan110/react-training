8ae73290250d61dd903acb4ec35df3dd504477b0
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-07-19 17:03:02 +0700

    Update routing product

--- practice-two/src/routes/index.jsx
+++ practice-two/src/routes/index.jsx
@@ -4,18 +4,22 @@ import { Routes, Route } from 'react-router-dom';
 // Import pages
 import Layout from '../layouts/LayoutMain';
 import Home from '../pages/Home';
+import Product from '../pages/Product';
+
+import { PAGES } from '../constants/route';
 
 const Routing = () => (
   <Routes>
-  <Route
-    path="/"
-    element={<Layout />}
-  >
     <Route
-      index
-      element={<Home />}
-    />
-  </Route>
-</Routes>
+      path="/"
+      element={<Layout />}
+    >
+      <Route
+        index
+        element={<Home />}
+      />
+      <Route path={PAGES.PRODUCT.PATH} element={<Product/>} />
+    </Route>
+  </Routes>
 );
 export default Routing;
