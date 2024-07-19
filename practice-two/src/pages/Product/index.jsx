361cb201d89284591e1ac63b847589324cec740b
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-07-19 17:02:30 +0700

    Update render product images

--- practice-two/src/pages/Product/index.jsx
+++ practice-two/src/pages/Product/index.jsx
@@ -1,8 +1,22 @@
-// import css
+import { useParams } from 'react-router-dom';
+
+import useFetchProduct from '../../hooks/useFetchProduct';
+
+import { getProductById } from '../../services/product-service';
+
 import './index.css';
 
+import ProductImages from '../../components/ProductImages';
+
 const Product = () => {
-  return <div>ProductDetail</div>;
+  const { id } = useParams();
+  const { products } = useFetchProduct(getProductById, id);
+
+  return (
+    <div>
+      <ProductImages images={products.image} />
+    </div>
+  );
 };
 
-export default Product
+export default Product;
