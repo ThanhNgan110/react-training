d9abe07738150d5320758030c6639f1a1554f88e
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-07-22 00:05:23 +0700

    Update page product detail

--- practice-two/src/pages/Product/index.jsx
+++ practice-two/src/pages/Product/index.jsx
@@ -1,20 +1,30 @@
 import { useParams } from 'react-router-dom';
-
 import useFetchProduct from '../../hooks/useFetchProduct';
-
 import { getProductById } from '../../services/product-service';
-
 import './index.css';
-
 import ProductImages from '../../components/ProductImages';
+import ProductContent from '../../components/ProductContent';
 
 const Product = () => {
   const { id } = useParams();
-  const { products } = useFetchProduct(getProductById, id);
+  const { products, loading, error } = useFetchProduct(getProductById, id);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error loading product</div>;
+  }
+
+  if (!products) {
+    return <div>No product found</div>;
+  }
 
   return (
-    <div>
+    <div className="d-flex section-product">
       <ProductImages images={products.image} />
+      <ProductContent products={products} />
     </div>
   );
 };
