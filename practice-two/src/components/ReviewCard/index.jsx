6aa5dbdb8778709cedd87a1ef1aaabd74e42c9ed
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-07-23 13:45:56 +0700

    Add component review card

---
+++ practice-two/src/components/ReviewCard/index.jsx
@@ -0,0 +1,37 @@
+import Image from '../Image';
+import Text from '../Text';
+import StarRating from '../StarRating';
+
+import './index.css';
+
+const ReviewCard = ({ reviews }) => {
+  const { imgUrl, name, ratting, comment, ratingDate } = reviews;
+  return (
+    <article className="d-flex flex-column gap-16 card-review">
+      <div className="d-flex gap-16">
+        <Image
+          src={imgUrl}
+          alt="avatar"
+        />
+        <div className="d-flex flex-column">
+          <Text as="h4">{name}</Text>
+          <StarRating starFull={ratting} />
+        </div>
+      </div>
+      <Text
+        as="p"
+        className="comment"
+      >
+        {comment}
+      </Text>
+      <Text
+        as="p"
+        className="date"
+      >
+        {ratingDate}
+      </Text>
+    </article>
+  );
+};
+
+export default ReviewCard;
