a58444472adca3c70695d4fefb276a524c7e059b
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-07-24 17:45:28 +0700

    Update layout

---
+++ practice-two/src/layouts/index.jsx
@@ -0,0 +1,18 @@
+// Import router
+import { Outlet } from 'react-router-dom';
+
+// Import pages
+import Header from './Header';
+import Footer from './Footer';
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
