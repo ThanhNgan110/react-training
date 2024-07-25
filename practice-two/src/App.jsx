11e56c97084b2d2405d7e1df0a2877e439babe6e
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-07-25 17:45:04 +0700

    Update app

--- practice-two/src/App.jsx
+++ practice-two/src/App.jsx
@@ -1,11 +1,54 @@
+import { useState } from 'react';
+// Import router
+import { Routes, Route } from 'react-router-dom';
+
 // Import css
 import './styles/index.css';
 
-// Import Routes
-import Routing from './routes/index';
+// Import pages
+import Layout from './layouts';
+import Home from './pages/Home';
+import Product from './pages/Product';
+import ModalReview from './components/ModalReview';
+
+import { PAGES } from './constants/route';
+
+import { ModalContext } from './context';
 
 const App = () => {
-  return <Routing />;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsOpen(true);
+  };
+
+  return (
+    <>
+      <ModalContext.Provider
+        value={{
+          isOpen,
+          handleOpenModal
+        }}
+      >
+        <ModalReview />
+        <Routes>
+          <Route
+            path="/"
+            element={<Layout />}
+          >
+            <Route
+              index
+              element={<Home />}
+            />
+            <Route
+              path={PAGES.PRODUCT.PATH}
+              element={<Product />}
+            />
+          </Route>
+        </Routes>
+      </ModalContext.Provider>
+    </>
+  );
 };
 
 export default App;
