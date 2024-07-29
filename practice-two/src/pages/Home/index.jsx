128dfaa49827ccb6a9a044f1b5a70ed572a7d11b
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-07-29 08:58:30 +0700

    Update home

--- practice-two/src/pages/Home/index.jsx
+++ practice-two/src/pages/Home/index.jsx
@@ -4,7 +4,7 @@ import SideBar from '../../layouts/SideBar';
 import Banner from './components/Banner';
 import Bar from '../../components/Bar';
 import ProductList from '../../components/ProductList';
-import Pagination from '../../components/Pagination';
+// import Pagination from '../../components/Pagination';
 
 import { OPTIONS } from '../../constants/label';
 
@@ -12,7 +12,8 @@ import './index.css';
 
 import {
   getProducts,
-  getProductSettings
+  getProductSettings,
+  filterProductByTradeMark
 } from '../../services/product-service';
 
 const Home = () => {
@@ -22,25 +23,35 @@ const Home = () => {
   ];
 
   const [count, setCount] = useState(0);
-
   const [products, setProducts] = useState([]);
-
   const [settings, setSettings] = useState({
     types: [],
     colors: [],
     maxPrice: 0
   });
 
+  const [price, setPrice] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
 
   const handlePageClick = pageCurrent => {
     setCurrentPage(pageCurrent);
   };
 
+  const handleFilterCategory = async category => {
+    const { data, error } = await filterProductByTradeMark(category);
+    if (!error) {
+      setProducts(data.products);
+      setCount(data.count);
+    }
+  };
+
+  const handleChangePrice = e => {
+    setPrice(Number(e.target.value));
+  };
+
   useEffect(() => {
     const handlePopulateProducts = async () => {
       const { data, error } = await getProducts();
-
       if (!error) {
         setCount(data.count);
         setProducts(data.products);
@@ -49,7 +60,6 @@ const Home = () => {
 
     const handlePopulateSettings = async () => {
       const { data, error } = await getProductSettings();
-
       if (!error) {
         setSettings(data);
       }
@@ -57,11 +67,16 @@ const Home = () => {
 
     handlePopulateSettings();
     handlePopulateProducts();
-  }, []);
+  }, []); // Only run once when the component mounts
 
   return (
     <div className="d-flex wrapper-content">
-      <SideBar settings={settings} />
+      <SideBar
+        settings={settings}
+        onClick={handleFilterCategory}
+        onChange={handleChangePrice}
+        price={price}
+      />
       <main>
         <Banner />
         <Bar
@@ -69,11 +84,11 @@ const Home = () => {
           count={count}
         />
         <ProductList products={products} />
-        <Pagination
+        {/* <Pagination
           count={count}
           currentPage={currentPage}
           onClick={handlePageClick}
-        />
+        /> */}
       </main>
     </div>
   );
