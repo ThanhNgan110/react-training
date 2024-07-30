d375eb12d5296a0550d6c20b39d4e03b715dd6d8
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-07-30 17:22:28 +0700

    Update api review

--- practice-two/src/services/review-service.js
+++ practice-two/src/services/review-service.js
@@ -1,11 +1,15 @@
-// import { URL_API } from '../constants/api';
-// import { get } from './api-service';
+import { URL_API } from '../constants/api';
+import { get, post } from './api-service';
 import { reviews } from '../mocks/product';
 
-/** Get data product */
-export const getReviews = async () => {
+/** Get data reviews by product by id */
+export const getReviewsByProductId = async productId => {
   return {
     data: reviews
   };
-  // return await get(URL_API.END_POINT_PRODUCTS);
+  // return await get(`${URL_API.END_POINT_REVIEWS}/${productId}`);
+};
+
+export const createReviews = async productId => {
+  return await post(`${URL_API.END_POINT_REVIEWS}/${productId}`);
 };
