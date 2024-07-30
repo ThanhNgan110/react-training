63c0364f8d22866d2ba47cde5ae28871602c24bd
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-07-30 17:21:56 +0700

    Add mock users

--- practice-two/src/mocks/product.js
+++ practice-two/src/mocks/product.js
@@ -1,130 +1,3 @@
-export const productsMock = {
-  products: [
-    {
-      id: '04798deb-586e-4006-92dc-11d852c850bc',
-      name: 'Nike Airmax 270 React',
-      price: 299.43,
-      image: [
-        'https://github.com/user-attachments/assets/2279bb8e-c59a-499f-8b79-e9410c13e22d',
-        'https://github.com/user-attachments/assets/c95ffe96-1617-4e43-b2be-d00b031f742c',
-        'https://github.com/user-attachments/assets/c95ffe96-1617-4e43-b2be-d00b031f742c'
-      ],
-      colors: ['006CFF', 'FC3E39', '171717', 'FFF600', 'FF00B4', 'EFDFDF'],
-      description:
-        'Nunc facilisis sagittis ullamcorper. Proin lectus ipsum, gravida et mattis vulputate, tristique ut lectus. Sed et lectus lorem nunc leifend laorevtr istique et congue. Vivamus adipiscin vulputate g nisl ut dolor',
-      category: 'Accessories',
-      size: ['S', 'M', 'L'],
-      type: 'Nike',
-      rating: 4,
-      reviews: '4'
-    },
-    {
-      id: '76465e8a-d08c-4046-ab7b-aa3c46513102',
-      name: 'Nike Airmax 270 React',
-      price: 299.43,
-      image: [
-        'https://github.com/user-attachments/assets/2279bb8e-c59a-499f-8b79-e9410c13e22d',
-        'https://github.com/user-attachments/assets/c95ffe96-1617-4e43-b2be-d00b031f742c'
-      ],
-      colors: ['006CFF', 'FC3E39', '171717', 'FFF600', 'FF00B4', 'EFDFDF'],
-      description:
-        'Nunc facilisis sagittis ullamcorper. Proin lectus ipsum, gravida et mattis vulputate, tristique ut lectus. Sed et lectus lorem nunc leifend laorevtr istique et congue. Vivamus adipiscin vulputate g nisl ut dolor',
-      category: 'Accessories',
-      size: ['S', 'M', 'L'],
-      type: 'Nike',
-      rating: 4,
-      reviews: '3'
-    },
-    {
-      id: 'f5f30785-673b-4c9d-9f1f-c66667ecb04b',
-      name: 'Nike Airmax 270 React',
-      price: 299.43,
-      image: [
-        'https://github.com/user-attachments/assets/2279bb8e-c59a-499f-8b79-e9410c13e22d',
-        'https://github.com/user-attachments/assets/c95ffe96-1617-4e43-b2be-d00b031f742c'
-      ],
-      colors: ['006CFF', 'FC3E39', '171717', 'FFF600', 'FF00B4', 'EFDFDF'],
-      description:
-        'Nunc facilisis sagittis ullamcorper. Proin lectus ipsum, gravida et mattis vulputate, tristique ut lectus. Sed et lectus lorem nunc leifend laorevtr istique et congue. Vivamus adipiscin vulputate g nisl ut dolor',
-      category: 'Accessories',
-      size: ['S', 'M', 'L'],
-      type: 'Airmax',
-      rating: 4,
-      reviews: '2'
-    },
-    {
-      id: 'd3030d7b-b635-4ca1-9a35-13dc597703d7',
-      name: 'Nike Airmax 270 React',
-      price: 299.43,
-      image: [
-        'https://github.com/user-attachments/assets/c95ffe96-1617-4e43-b2be-d00b031f742c',
-        'https://github.com/user-attachments/assets/2279bb8e-c59a-499f-8b79-e9410c13e22d'
-      ],
-      colors: ['006CFF', 'FC3E39', '171717', 'FFF600', 'FF00B4', 'EFDFDF'],
-      description:
-        'Nunc facilisis sagittis ullamcorper. Proin lectus ipsum, gravida et mattis vulputate, tristique ut lectus. Sed et lectus lorem nunc leifend laorevtr istique et congue. Vivamus adipiscin vulputate g nisl ut dolor',
-      category: 'Accessories',
-      size: ['S', 'M', 'L'],
-      type: 'Vans',
-      rating: 2,
-      reviews: '1'
-    }
-  ],
-  count: 5
-};
-
-export const productSettingsMock = {
-  types: [
-    {
-      name: 'Airmax',
-      count: 48
-    },
-    {
-      name: 'Nike',
-      count: 14
-    },
-    {
-      name: 'Vans',
-      count: 23
-    },
-    {
-      name: 'Adidas',
-      count: 95
-    },
-    {
-      name: 'All Stars',
-      count: 1
-    }
-  ],
-  colors: ['006CFF', 'FC3E39', '171717', 'FFF600', 'FF00B4', 'EFDFDF'],
-  maxPrice: 51
-};
-
-export const productMock = {
-  id: '04798deb-586e-4006-92dc-11d852c850bc',
-  name: 'Nike Airmax 270 React',
-  price: 299.43,
-  image: [
-    'https://github.com/user-attachments/assets/2279bb8e-c59a-499f-8b79-e9410c13e22d',
-    'https://github.com/user-attachments/assets/c95ffe96-1617-4e43-b2be-d00b031f742c',
-    'https://github.com/user-attachments/assets/c95ffe96-1617-4e43-b2be-d00b031f742c',
-    'https://github.com/user-attachments/assets/c95ffe96-1617-4e43-b2be-d00b031f742c'
-  ],
-  colors: ['006CFF', 'FC3E39', '171717'],
-  description:
-    'Nunc facilisis sagittis ullamcorper. Proin lectus ipsum, gravida et mattis vulputate, tristique ut lectus. Sed et lectus lorem nunc leifend laorevtr istique et congue. Vivamus adipiscin vulputate g nisl ut dolor',
-  category: 'Accessories',
-  size: [
-    { value: 's', name: 'S' },
-    { value: 'm', name: 'M' },
-    { value: 'l', name: 'L' }
-  ],
-  orderStatus: 'In Stock',
-  type: 'Nike',
-  reviews: 4,
-  rating: 3
-};
-
 export const reviews = [
   {
     imgUrl:
@@ -145,5 +18,36 @@ export const reviews = [
       'air max are always very comfortable fit, clean and just perfect in every way. just the box was too small and scrunched the sneakers up a little bit, not sure if the box was always this small but the 90s are and will always be one of my favorites.',
     ratingDate: '2024-07-24T04:07:43.07Z',
     id: '04798deb-586e-4006-92dc-11d852c850dc'
+  }
+];
+
+export const users = [
+  {
+    id: 1,
+    name: 'Anh Nguyen',
+    avatar:
+      'https://github.com/user-attachments/assets/a6b92232-6256-4894-adb8-78db03904281',
+    userName: 'anh.nguyen'
+  },
+  {
+    id: 2,
+    name: 'Minh Dinh Nguyen',
+    avatar:
+      'https://github.com/user-attachments/assets/a6b92232-6256-4894-adb8-78db03904281',
+    userName: 'minh.dinh'
+  },
+  {
+    id: 3,
+    name: 'Do Nguyen',
+    avatar:
+      'https://github.com/user-attachments/assets/a6b92232-6256-4894-adb8-78db03904281',
+    userName: 'do.nguyen'
+  },
+  {
+    id: 4,
+    name: 'Ngan Thai',
+    avatar:
+      'https://github.com/user-attachments/assets/f3bf0119-8057-45e1-bb61-41be8e184905',
+    userName: 'ngan.thai'
   }
 ];
