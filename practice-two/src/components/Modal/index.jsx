5c6cdd64ab17b725488346f770b579ba43b0e961
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-07-31 16:02:23 +0700

    Update modal review

--- practice-two/src/components/Modal/index.jsx
+++ practice-two/src/components/Modal/index.jsx
@@ -1,21 +1,23 @@
 import clsx from 'clsx';
 import './index.css';
 
-const Modal = ({ className, modalHeader, modalBody, modalFooter }) => {
+const Modal = ({ open, className, modalHeader, modalBody, modalFooter }) => {
+  if (!open) {
+    return null;
+  }
+
   return (
-    <>
-      <div
-        className={clsx('modal-overlay', {
-          [className]: !!className
-        })}
-      >
-        <div className="modal-content">
-          <div className="modal-header">{modalHeader}</div>
-          <div className="modal-body">{modalBody}</div>
-          <div className="modal-footer">{modalFooter}</div>
-        </div>
+    <div
+      className={clsx('modal-overlay', {
+        [className]: !!className
+      })}
+    >
+      <div className="modal-content">
+        <div className="modal-header">{modalHeader}</div>
+        <div className="modal-body">{modalBody}</div>
+        <div className="modal-footer">{modalFooter}</div>
       </div>
-    </>
+    </div>
   );
 };
 
--- practice-two/src/components/ReviewDialog/index.jsx
+++ practice-two/src/components/ReviewDialog/index.jsx
@@ -1,4 +1,4 @@
-import { useState} from 'react';
+import { useState } from 'react';
 
 import Modal from '../Modal';
 import Text from '../Text';
@@ -8,99 +8,86 @@ import StarRating from '../StarRating';
 
 import './index.css';
 
-import { createReviews } from '../../services/review-service';
-
-import { users } from '../../mocks/users';
-
-import { getRandomUser } from '../../utils/randomUser';
-
-
-const ModalReview = () => {
-  const [rating, setRating] = useState(0);
-  const [review, setReview] = useState('');
+const ReviewDialog = ({ open, onSubmit, onClose }) => {
+  const [rating, setRating] = useState(5);
+  const [comment, setComment] = useState('');
 
   const handleChangeRating = numberRating => {
     setRating(numberRating);
   };
 
-  const handleReviewChange = e => {
-    setReview(e.target.value);
+  const handleChangeComment = e => {
+    setComment(e.target.value);
   };
 
-  const onSubmitReview = async e => {
-    console.log('productId');
-    e.preventDefault();
-    const userReview = {
-      rating,
-      comment: review,
-      userName: users[getRandomUser(users.length)],
-      productId:""
-    };
-    await createReviews(userReview);
+  const handleSubmit = () => {
+    if (typeof onSubmit === 'function') {
+      onSubmit({
+        rating,
+        comment
+      });
+    }
   };
 
   return (
-    <>
-        <Modal
-          className={''}
-          modalHeader={
-            <div className="header-group">
-              <Text
-                as="p"
-                className="text-info fw-semibold fs-1"
-              >
-                Review
-              </Text>
-              <Button
-                iconClassName="btn-close icon-close"
-                onClick={''}
-              />
-            </div>
-          }
-          modalBody={
-            <>
-              <form
-                className="form"
-                onSubmit={onSubmitReview}
-              >
-                <Text
-                  as="p"
-                  className="label"
-                >
-                  Please write about your level of satisfaction with your
-                  product
-                </Text>
-                <StarRating
-                  width={32}
-                  height={32}
-                  className="d-flex gap-20 justify-content-center"
-                  rating={rating}
-                  onClick={handleChangeRating}
-                />
-                <Text
-                  as="p"
-                  className="label"
-                >
-                  Write your review here
-                </Text>
-                <Textarea
-                  className="textarea-size"
-                  placeholder="Write your review here"
-                  value={review}
-                  onChange={handleReviewChange}
-                />
-                <Button
-                  className="btn-review"
-                  type="submit"
-                >
-                  Submit
-                </Button>
-              </form>
-            </>
-          }
-        />
-    </>
+    <Modal
+      open={open}
+      modalHeader={
+        <div className="header-group">
+          <Text
+            as="p"
+            className="text-info fw-semibold fs-1"
+          >
+            Review
+          </Text>
+          <Button
+            iconClassName="btn-close icon-close"
+            onClick={onClose}
+          />
+        </div>
+      }
+      modalBody={
+        <>
+          <form
+            className="form"
+            onSubmit={handleSubmit}
+          >
+            <Text
+              as="p"
+              className="label"
+            >
+              Please write about your level of satisfaction with your product
+            </Text>
+            <StarRating
+              width={32}
+              height={32}
+              className="d-flex gap-20 justify-content-center"
+              rating={rating}
+              onClick={handleChangeRating}
+            />
+            <Text
+              as="p"
+              className="label"
+            >
+              Write your review here
+            </Text>
+            <Textarea
+              className="textarea-size"
+              placeholder="Write your review here"
+              value={comment}
+              onChange={handleChangeComment}
+            />
+            <Button
+              className="btn-review"
+              type="submit"
+            >
+              Submit
+            </Button>
+          </form>
+        </>
+      }
+    />
   );
 };
 
-export default ModalReview;
+export default ReviewDialog;
