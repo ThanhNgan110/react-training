b8576e224609809947f9db8f5a8014a393ebe8f2
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-08-01 11:26:42 +0700

    Update handle submit reviews

--- practice-two/src/pages/Product/index.jsx
+++ practice-two/src/pages/Product/index.jsx
@@ -10,23 +10,35 @@ import Tab from '../../components/Tabs/Tab';
 import TabContent from '../../components/Tabs/TabContent';
 import TabBar from '../../components/TabBar';
 import Loading from '../../components/Spinner';
+import Toast from '../../components/Toast';
+import ReviewDialog from '../../components/ReviewDialog';
 
 import { PAGES } from '../../constants/route';
 
 import { TAB } from '../../constants/tab';
 
-import { reviews } from '../../mocks/product';
 import { getProductById } from '../../services/product-service';
+import {
+  createReviews,
+  getReviewsByProductId
+} from '../../services/review-service';
 
-import ReviewDialog from '../../components/ReviewDialog';
+import { getRandomInt } from '../../utils/randomUser';
+
+import { users } from '../../mocks/users';
+
+import useToast from '../../hooks/useToast';
 
 const Product = () => {
   let { id } = useParams();
 
-  // mock data
-  const review = reviews;
+  const { showToast, alert } = useToast(); // Use the custom hook for toast notifications
+
   const [product, setProduct] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [isOpenReviewDialog, setOpenReviewDialog] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState('');
+  const [reviews, setReview] = useState(null);
 
   const [currentTab, setCurrentTab] = useState(0);
 
@@ -37,19 +49,58 @@ const Product = () => {
     { title: product ? product.name : '', href: '', active: true }
   ];
 
-  const handleSetTab = index => {
+  const handleSetTab = async index => {
     setCurrentTab(index);
+    // default tab first click tab not call api
+    if (index !== 0) {
+      fetchGetReviews();
+    }
+  };
+
+  const fetchGetReviews = async () => {
+    const { data } = await getReviewsByProductId(id);
+    setReview(data);
+  };
+
+  const handleCloseReviewDialog = () => {
+    setOpenReviewDialog(false);
+  };
+
+  const handleOpenReviewDialog = id => {
+    setSelectedProduct(id);
+    setOpenReviewDialog(true);
+  };
+
+  const fetchProductById = async () => {
+    setIsLoading(true);
+    const { data, error } = await getProductById(id);
+    if (!error) {
+      setProduct(data);
+    }
+    setIsLoading(false);
+  };
+
+  const handleSubmitReview = async ({ rating, comment }) => {
+    const { error } = await createReviews({
+      rating,
+      comment,
+      userName: users[getRandomInt(users.length)],
+      productId: selectedProduct
+    });
+
+    if (error) {
+      showToast('Review submitted failed!', 'danger');
+    }
+
+    showToast('🦄 Review submitted successfully!', 'success');
+    setOpenReviewDialog(false);
+    // call fetch product by id
+    await fetchProductById();
+    const { data: reviews } = await getReviewsByProductId(id);
+    setReview(reviews);
   };
 
   useEffect(() => {
-    const fetchProductById = async () => {
-      setIsLoading(true);
-      const { data, error } = await getProductById(id);
-      if (!error) {
-        setProduct(data);
-      }
-      setIsLoading(false);
-    };
     fetchProductById();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -60,7 +111,6 @@ const Product = () => {
 
   return (
     <>
-    <ReviewDialog/>
       <TabBar items={items} />
       <main className="wrapper-product">
         <section className="d-flex gap-40 section-product">
@@ -68,7 +118,10 @@ const Product = () => {
             images={product.image}
             name={product.name}
           />
-          <ProductContent product={product} />
+          <ProductContent
+            product={product}
+            onOpen={handleOpenReviewDialog}
+          />
         </section>
         <section className="tab-review">
           <Tabs>
@@ -85,12 +138,23 @@ const Product = () => {
             </div>
             <TabContent
               currentTab={currentTab}
-              reviews={review}
+              reviews={reviews ?? null}
               description={product.description}
             />
           </Tabs>
         </section>
       </main>
+      <ReviewDialog
+        open={isOpenReviewDialog}
+        onClose={handleCloseReviewDialog}
+        onSubmit={handleSubmitReview}
+      />
+      {alert.show && (
+        <Toast
+          type={alert.type}
+          msg={alert.msg}
+        />
+      )}
     </>
   );
 };
