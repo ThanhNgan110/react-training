cad776d574118c543633056f29072014ae776aab
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-08-01 11:14:17 +0700

    Update api get review

--- practice-two/src/services/review-service.js
+++ practice-two/src/services/review-service.js
@@ -1,13 +1,9 @@
 import { URL_API } from '../constants/api';
 import { get, post } from './api-service';
-import { reviews } from '../mocks/product';
 
 /** Get data reviews by product by id */
 export const getReviewsByProductId = async productId => {
-  return {
-    data: reviews
-  };
-  // return await get(`${URL_API.END_POINT_REVIEWS}/${productId}`);
+  return await get(`${URL_API.END_POINT_REVIEWS}/${productId}`);
 };
 
 /** Post data reviews by product by id */
