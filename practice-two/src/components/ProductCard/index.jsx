74fcb24da3eabd0d14d027e9c927b9e4c34811ca
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-08-02 08:45:33 +0700

    Update product card

--- practice-two/src/components/ProductCard/index.jsx
+++ practice-two/src/components/ProductCard/index.jsx
@@ -15,81 +15,83 @@ const ProductCard = ({ product, onOpen }) => {
     product;
 
   return (
-    <article className="d-flex divider">
-      <div className="box-image">
-        <Link
-          className="box-image"
-          key={id}
-          to={`/products/${id}`}
-        >
+    <Link
+      key={id}
+      to={`/products/${id}`}
+    >
+      <article className="d-flex divider">
+        <div className="box-image">
           <Image
             src={images[0] ?? ''}
             alt={`product ${name}`}
           />
-        </Link>
 
-        <span className="badge"></span>
-      </div>
-      <div className="d-flex flex-column product-content">
-        <Text
-          as="h3"
-          variant="primary"
-        >
-          {name ? name : ''}
-        </Text>
-        <Divider>
+          <span className="badge"></span>
+        </div>
+        <div className="d-flex flex-column product-content">
+          <Text
+            as="h3"
+            variant="primary"
+          >
+            {name ? name : ''}
+          </Text>
+          <Divider>
+            <div className="d-flex align-items-center gap-10">
+              <StarRating
+                rating={aveRating}
+                width="12"
+                height="12"
+              />
+              <Text
+                as="p"
+                className="fs-4 text-gray"
+              >
+                {reviewCount ?? 0} reviews
+              </Text>
+              <Button
+                variant="link"
+                onClick={e => {
+                  e.preventDefault();
+                  onOpen(id);
+                }}
+              >
+                Submit a review
+              </Button>
+            </div>
+          </Divider>
+
           <div className="d-flex align-items-center gap-10">
-            <StarRating
-              rating={aveRating}
-              width="12"
-              height="12"
-            />
             <Text
               as="p"
-              className="fs-4 text-gray"
+              className="fs-3 text-info fw-bold"
             >
-              {reviewCount ?? 0} reviews
+              {price ? formatPrice(price) : ''}
             </Text>
-            <Button
-              variant="link"
-              onClick={() => onOpen(id)}
-            >
-              Submit a review
-            </Button>
+            <div className="d-flex group">
+              <Text
+                as="p"
+                className="fs-5 fw-normal text-decoration-line-through text-line-through"
+              >
+                $534,33
+              </Text>
+              <Text
+                as="p"
+                className="fs-5 fw-bold text-pink"
+              >
+                24% Off
+              </Text>
+            </div>
           </div>
-        </Divider>
-
-        <div className="d-flex align-items-center gap-10">
           <Text
             as="p"
-            className="fs-3 text-info fw-bold"
+            variant="secondary"
+            ellipsis
           >
-            {price ? formatPrice(price) : ''}
+            {description}
           </Text>
-          <div className="d-flex group">
-            <Text
-              as="p"
-              className="fs-5 fw-normal text-decoration-line-through text-line-through"
-            >
-              $534,33
-            </Text>
-            <Text
-              as="p"
-              className="fs-5 fw-bold text-pink"
-            >
-              24% Off
-            </Text>
-          </div>
         </div>
-        <Text
-          as="p"
-          variant="secondary"
-          ellipsis
-        >
-          {description}
-        </Text>
-      </div>
-    </article>
+      </article>
+    </Link>
   );
 };
 
