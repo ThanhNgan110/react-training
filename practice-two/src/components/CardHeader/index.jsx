d4ea1a1e1c7924ac6b4413f7aaf045ca0028acd5
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-08-07 10:37:36 +0700

    Add component card header

---
+++ practice-two/src/components/CardHeader/index.jsx
@@ -0,0 +1,42 @@
+import Text from '../Text';
+import StarRating from '../StarRating';
+import Button from '../Button';
+
+const CardHeader = ({ id, name, averageRating, reviewsCount, onOpen }) => {
+  return (
+    <>
+      <Text
+        as="h2"
+        variant="primary"
+      >
+        {name ? name : ''}
+      </Text>
+      <div className="d-flex flex-column">
+        <div className="d-flex align-items-center gap-10 divider">
+          <StarRating
+            rating={averageRating}
+            width="12"
+            height="12"
+          />
+          <Text
+            as="p"
+            className="fs-4 text-gray"
+          >
+            {reviewsCount ?? 0} reviews
+          </Text>
+          <Button
+            variant="link"
+            onClick={e => {
+              e.preventDefault();
+              onOpen(id);
+            }}
+          >
+            Submit a review
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CardHeader;
