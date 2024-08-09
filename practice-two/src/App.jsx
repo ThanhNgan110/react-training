be699f04db489674f20731b08841b9f713ea630a
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-08-09 11:09:56 +0700

    Update handle loading

--- practice-two/src/App.jsx
+++ practice-two/src/App.jsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 // Import router
 import { Routes, Route } from 'react-router-dom';
 
@@ -10,6 +11,8 @@ import Home from './pages/Home';
 import Product from './pages/Product';
 import { PAGES } from './constants/route';
 
+import Loading from './components/Loading';
+
 const App = () => {
   return (
     <>
@@ -20,7 +23,11 @@ const App = () => {
         >
           <Route
             index
-            element={<Home />}
+            element={
+              <Suspense fallback={<Loading />}>
+                <Home />
+              </Suspense>
+            }
           />
           <Route
             path={PAGES.PRODUCT.PATH}
