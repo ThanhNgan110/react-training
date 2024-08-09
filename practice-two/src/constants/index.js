45a5788caea896246680f6c444750430a44bd57f
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-08-09 17:22:36 +0700

    Add define constant

---
+++ practice-two/src/constants/index.js
@@ -0,0 +1,36 @@
+const TAB = {
+  PRODUCT_INFO: 'Product Information',
+  REVIEWS: 'Reviews'
+};
+
+const TABS = [{ title: TAB.PRODUCT_INFO }, { title: TAB.REVIEWS }];
+
+const OPTIONS = {
+  NAME: 'Name',
+  PRICE: 'Price'
+};
+
+const SORT_DATA = [OPTIONS.NAME, OPTIONS.PRICE];
+
+const FOOTER_LIST_INFORMATION = [
+  'Information',
+  'About us',
+  'Privacy Policy',
+  'Terms & Conditions'
+];
+
+const ICON = {
+  ICON_CARD_MASTER: 'icon-card-master',
+  ICON_CARD_PAYPAL: 'icon-card-paypal',
+  ICON_CARD_SHAPE: 'icon-card-shape',
+  ICON_CARD_UNION: 'icon-card-union'
+};
+
+const LIST_ICON = [
+  ICON.ICON_CARD_MASTER,
+  ICON.ICON_CARD_PAYPAL,
+  ICON.ICON_CARD_SHAPE,
+  ICON.ICON_CARD_UNION
+];
+
+export { TABS, SORT_DATA, FOOTER_LIST_INFORMATION, LIST_ICON };
