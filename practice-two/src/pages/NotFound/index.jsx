f304cf5fe07b3a8b7719186b770da74461804b4c
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-08-14 09:29:53 +0700

    Add page not found

--- practice-two/src/pages/NotFound/index.jsx
+++ practice-two/src/pages/NotFound/index.jsx
@@ -1,5 +1,26 @@
+import { Link } from 'react-router-dom';
+
+import Text from '../../components/Text';
+
+import './index.css';
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <section className="section">
+      <Text
+        as="p"
+        className="page"
+      >
+        Ooops!!! The page you are looking for is not found
+      </Text>
+      <Link
+        className="back-home"
+        to="/"
+      >
+        Back to home
+      </Link>
+    </section>
+  );
 };
 
 export default NotFound;
