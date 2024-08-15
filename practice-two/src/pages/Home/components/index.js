95b986bb07cd0a4055a811a2758824ae77d044b1
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-08-15 11:19:18 +0700

    Add index for export files

---
+++ practice-two/src/pages/Home/components/index.js
@@ -0,0 +1,4 @@
+export { default as Banner } from './Banner';
+export { default as Bar } from './Bar';
+export { default as ProductList } from './ProductList';
+export { default as SideBar } from './SideBar';
